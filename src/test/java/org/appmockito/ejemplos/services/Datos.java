69b50f47dde952a7e5c68b890b3e01c001aafb01
Cristian Blandon <cristianblandon13@gmail.com>
2021-09-13 22:35:52 -0500

    Added new dependencies and implemented them

---
+++ src/test/java/org/appmockito/ejemplos/services/Datos.java
@@ -0,0 +1,13 @@
+package org.appmockito.ejemplos.services;
+
+import org.appmockito.ejemplos.models.Examen;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Datos {
+    public static final List<Examen> EXAMENES = Arrays.asList(new Examen(5L, "Matemáticas"), new Examen(6L, "Lenguaje"),
+            new Examen(7L, "Historia"));
+
+    public static final List<String> PREGUNTAS = Arrays.asList("aritmética", "integrales", "derivadas", "trigonomentría", "geometría");
+}
--- src/test/java/org/appmockito/ejemplos/services/ExamenServiceImplTest.java
+++ src/test/java/org/appmockito/ejemplos/services/ExamenServiceImplTest.java
@@ -6,12 +6,12 @@ import org.appmockito.ejemplos.models.Examen;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -30,10 +30,7 @@ class ExamenServiceImplTest {
 
     @Test
     void findExamenPorNombre() {
-
-        List<Examen> datos = Arrays.asList(new Examen(5L, "Matemáticas"), new Examen(6L, "Lenguaje"),
-                new Examen(7L, "Historia"));
-        when(examenDao.findAll()).thenReturn(datos);
+        when(examenDao.findAll()).thenReturn(Datos.EXAMENES);
         Optional<Examen> examen = service.findExamenPorNombre("Matemáticas");
         assertTrue(examen.isPresent());
         assertEquals(5, examen.orElseThrow(ArithmeticException::new).getId());
@@ -46,5 +43,14 @@ class ExamenServiceImplTest {
         when(examenDao.findAll()).thenReturn(datos);
         Optional<Examen> examen = service.findExamenPorNombre("Matemáticas");
         assertFalse(examen.isPresent());
+    }
+
+    @Test
+    void testPreguntasExamen() {
+        when(examenDao.findAll()).thenReturn(Datos.EXAMENES);
+        when(preguntaDao.findPreguntasPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
+        Examen examen = service.findExamenPorNombreConPreguntas("Matemáticas");
+        assertEquals(5, examen.getPreguntas().size());
+        assertTrue(examen.getPreguntas().contains("aritmética"));
     }
 }
